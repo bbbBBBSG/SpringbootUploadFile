c8f1f5d60c9c0f6d83e8289b0d93306e3dae00ba
BigSea001 <936499719@qq.com>
2019-04-03 13:51:14 +0800

    跨域

--- src/main/java/com/dahai/file/UploadFilePathConfig.java
+++ src/main/java/com/dahai/file/UploadFilePathConfig.java
@@ -2,6 +2,7 @@ package com.dahai.file;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -16,5 +17,14 @@ public class UploadFilePathConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(staticAccessPath).addResourceLocations("file:" + uploadFolder);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
     }
 }
